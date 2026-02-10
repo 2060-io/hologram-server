00686d1db524d754c3181b234eb40b97888b58bf
andres vallecilla <andresfelipe083195@hotmail.com>
2026-02-10 17:44:28 -0500

    feat: linter

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/no-cycle': 'error',
+    'import/export': 'off',
+    'import/no-unresolved': 'off',
+    'import/newline-after-import': ['error', { count: 1 }],
+    'import/order': [
+      'error',
+      {
+        groups: ['type', ['builtin', 'external'], 'parent', 'sibling', 'index'],
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['apps/**/*.{js,ts,tsx}'],
+      rules: {
+        'no-console': 'error',
+      },
+    },
+  ]
+}
